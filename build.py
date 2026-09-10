6ee90bbe024679d96c679a602349e5232e0fdccd
DNSCond <condi7002@gmail.com>
2026-09-10 19:13:37 +0200

    fix-icon

--- build.py
+++ build.py
@@ -7,12 +7,13 @@ fetch_queue = (
     ('require/head2/ANTStylesheet.css', 'require/head2/ANTStylesheet.css',),
     ('require/JSONScript.js', 'require/JSONScript.js',),
     ('require/head2/domContentLoadedPromise.js', 'require/head2/domContentLoadedPromise.js',),
+    ('gallery/favicon.ico', 'gallery/favicon.ico',),
 )
 basepath = pathlib.Path(r'D:\var\www\BOTs\dnscond.github.io')
 
 for i in fetch_queue:
     path = basepath / i[1]
-    url = f'http://localhost/{i[0]}?toGithub=1'
+    url = f'http://localhost/{i[0]}?isntLocalhost=1'
     resp = requests.get(url)
     path = path.with_suffix('.html') if path.suffix == '.php' else path
     path.parent.mkdir(parents=True, exist_ok=True)
